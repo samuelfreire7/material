d4a3d6037760eec9aecd92dbbf8c68697911292e
Samuel Freire Gomes <samuelgomes0570@digitalcollege.internal>
2025-04-25 16:40:29 -0300

    Atualização 1.1 do dia 25/04

---
+++ Samuel/Módulo-1/HTML/25-04/function.js
@@ -0,0 +1,12 @@
+// function saudacao() {
+//     console.log("Olá Mundo");
+// }
+
+// saudacao();
+
+function saudacao(nome, idade, nacionalidade) {   
+
+    console.log("Olá " +nome+ ", você tem: " +idade+ " anos e sua nacionalidade é: " +nacionalidade);
+}
+
+saudacao("Samuel",  17, "brasileiro")
