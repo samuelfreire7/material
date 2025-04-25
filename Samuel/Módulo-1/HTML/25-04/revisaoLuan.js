08baab7d8d556d5ded97355371d07e2864b6434f
Samuel Freire Gomes <samuelgomes0570@digitalcollege.internal>
2025-04-25 15:41:25 -0300

    Atualização 1 do dia 25/04

---
+++ Samuel/Módulo-1/HTML/25-04/revisaoLuan.js
@@ -0,0 +1,13 @@
+let nome = "Samuel";
+let idade = 17;
+let nacionalidade = "Brasileiro";
+
+// console.log("Eu me chamo "+nome+", tenho "+idade+" anos e sou "+nacionalidade)
+ let eAluno = true
+ let curso = "full Stack";
+
+ if (eAluno == true) {
+    console.log("Olá, me chamo "+nome+" e sou aluno do curso "+curso);
+ } else {
+    console.log("Olá, me chamo "+nome+" e sou professor do curso "+curso);
+ }
---
+++ Samuel/Módulo-1/HTML/25-04/revisaoLuan2.js
@@ -0,0 +1,16 @@
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let frutas = ["Laranja", "Maçâ", "Uva"];
+
+// console.log(frutas[0]);
+// console.log(frutas.length);
+// console.log(frutas[frutas.length -1]);
+
+let frutas = ["Laranja", "Maçâ", "Uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
