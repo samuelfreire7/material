ae38169411a43284ee3862661c76121fbcce52c2
samuelfreire7 <samuelfgwm@gmail.com>
2025-05-06 16:50:34 -0300

    Atualização desafios functions

--- Samuel/Módulo-2/JAVA SCRIPT/06-05/revisaoFunction.js
+++ Samuel/Módulo-2/JAVA SCRIPT/06-05/revisaoFunction.js
@@ -1,62 +1,62 @@
 //DESAFIO 1
 // function saudacao() {
-//     console.log("Olá, seja bem-vindo!");
+//    return "Olá, seja bem-vindo!";
 // }
-// saudacao()
+// console.log(saudacao())
 
 //DESAFIO 2
 // let nome = "Samuel";
 // function hello(nome) {
-//     console.log("Olá, " +nome+ "!");
+//     return "Olá, " +nome+ "!";
 // }
-// hello(nome)
+// console.log(hello(nome))
 
 //DESAFIO 3
 // function soma(numero1,numero2) {
 //     let soma = numero1 + numero2;
 
-//     console.log("soma: " +soma);
+//     return "soma: " +soma;
 // }
-// soma(2, 2)
+// console.log(soma(2, 2))
 
 //DESAFIO 4
 // function par(numero) {
 //     if(numero % 2 ==0) {
-//         console.log("O número é par.");
+//         return true
 //     } else {
-//         console.log("O número é impar.");
+//         return false
 //     }
 // }
-// par(10)
+// console.log(par(10));
 
 //DESAFIO 5
 // function media(n1, n2, n3) {
 //     let mediaFinal = (n1 + n2 +n3)/3
 
 //     if(mediaFinal >= 7) {
-//         console.log("Aprovado!");
+//         return "Aprovado!";
 //     } else {
-//         console.log("Recuperção ou Reprovação!");
+//         return "Recuperção ou Reprovação!";
 //     }
 // }
-// media(7, 7, 7)
+// console.log(media(7, 7, 7))
 
 //DESAFIO 6
 // function dobro(numero) {
 //     let dobro = numero * 2;
 
-//     console.log("O dobro desse número é: " +dobro+ ".");
+//     return "O dobro desse número é: " +dobro+ ".";
 // }
-// dobro(7)
+// console.log(dobro(7))
 
 //DESAFIO 7 
 // function maior(n1, n2) {
 //     if(n1 > n2){
-//         console.log("O " +n1+ " é maior.");
+//         return "O " +n1+ " é maior.";
 //     } else if (n1 == n2){
-//         console.log("Os números são iguais.");
+//         return "Os números são iguais.";
 //     } else {
-//         console.log("O " +n2+ " é maior.");
+//         return "O " +n2+ " é maior.";
 //     }
 // }
-// maior(7, 5)
+// console.log(maior(7, 5))
