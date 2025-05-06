a7233cde7a4668124f8b63df8b983cd9489d86bb
samuelfreire7 <samuelfgwm@gmail.com>
2025-05-06 16:16:13 -0300

    Desafios Function

---
+++ Samuel/Módulo-2/JAVA SCRIPT/06-05/revisaoFunction.js
@@ -0,0 +1,62 @@
+//DESAFIO 1
+// function saudacao() {
+//     console.log("Olá, seja bem-vindo!");
+// }
+// saudacao()
+
+//DESAFIO 2
+// let nome = "Samuel";
+// function hello(nome) {
+//     console.log("Olá, " +nome+ "!");
+// }
+// hello(nome)
+
+//DESAFIO 3
+// function soma(numero1,numero2) {
+//     let soma = numero1 + numero2;
+
+//     console.log("soma: " +soma);
+// }
+// soma(2, 2)
+
+//DESAFIO 4
+// function par(numero) {
+//     if(numero % 2 ==0) {
+//         console.log("O número é par.");
+//     } else {
+//         console.log("O número é impar.");
+//     }
+// }
+// par(10)
+
+//DESAFIO 5
+// function media(n1, n2, n3) {
+//     let mediaFinal = (n1 + n2 +n3)/3
+
+//     if(mediaFinal >= 7) {
+//         console.log("Aprovado!");
+//     } else {
+//         console.log("Recuperção ou Reprovação!");
+//     }
+// }
+// media(7, 7, 7)
+
+//DESAFIO 6
+// function dobro(numero) {
+//     let dobro = numero * 2;
+
+//     console.log("O dobro desse número é: " +dobro+ ".");
+// }
+// dobro(7)
+
+//DESAFIO 7 
+// function maior(n1, n2) {
+//     if(n1 > n2){
+//         console.log("O " +n1+ " é maior.");
+//     } else if (n1 == n2){
+//         console.log("Os números são iguais.");
+//     } else {
+//         console.log("O " +n2+ " é maior.");
+//     }
+// }
+// maior(7, 5)
